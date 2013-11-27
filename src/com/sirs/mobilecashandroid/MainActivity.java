e8cb31755ac49e269e3b9be2dabfc1179cc7af11
Samuel Coelho <samuelfcmc@gmail.com>
2013-11-27 16:54:55 +0000

    Implemented the functionality to buy a product

--- src/com/sirs/mobilecashandroid/MainActivity.java
+++ src/com/sirs/mobilecashandroid/MainActivity.java
@@ -2,8 +2,15 @@ package com.sirs.mobilecashandroid;
 
 import java.io.IOException;
 
-import comsirs.mobilecashandroid.R;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.entity.StringEntity;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import comsirs.mobilecashandroid.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
@@ -22,8 +29,38 @@ public class MainActivity<T> extends Activity {
 	private EditText product;
 	private TextView responseTxt;
 	
-	private void buyProduct() throws IOException {
-
+	private void buyProduct() throws IOException, JSONException {
+		AsyncHttpClient client = new AsyncHttpClient();
+		
+		String username = userName.getText().toString();
+		String password = passwd.getText().toString();
+		String productCode = product.getText().toString();
+		
+		JSONObject jsonParams = new JSONObject();
+        jsonParams.put("username", username);
+        jsonParams.put("password", password);
+        jsonParams.put("product", productCode);
+        StringEntity entity = new StringEntity(jsonParams.toString());
+        String url = "http://mobilecashserver.herokuapp.com/api/buy";
+        client.post(getApplicationContext(), url, entity, "application/json", new AsyncHttpResponseHandler() {
+        	@Override
+        	public void onSuccess(String response) {
+        		try {
+					JSONObject object = new JSONObject(response);
+					responseTxt.setText("You have bougth a " + object.getString("product") + " and you have " + object.getString("balance"));
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					try {
+						JSONObject object = new JSONObject(response);
+						responseTxt.setText(object.getString("message"));
+					} catch (JSONException e1) {
+						responseTxt.setText("FATAL ERROR!!");
+					}
+				}
+        		
+        	}
+        });
 	}
 	
 	
@@ -40,6 +77,9 @@ public class MainActivity<T> extends Activity {
 				try {
 					buyProduct();
 				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (JSONException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
